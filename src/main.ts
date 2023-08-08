f1163700c2576dbf6f1cad4880a461432cee596e
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-08-08 12:26:02 +0200

    Preparacion componentes busqueda y debounce manual

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken = 'pk.eyJ1IjoiYW91cmlhZ2hsaSIsImEiOiJjbGttZzFhMm0wZXZjM3FwbHdtMWRwd2lvIn0.dr0ZtDVD14QyeTyX3UmeyA';
+
+
+if( !navigator.geolocation){
+  alert('Navegador no soporta la Geolocation');
+  throw new Error('Navegador no soporta la Geolocation');
+}
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
